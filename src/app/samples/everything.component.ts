ef4620307fc2758fd4b2fee2b3d04557f7dfbfdb
Andrew Landsverk <dewdew@gmail.com>
2018-01-19 13:23:33 -0600

    0.0.27 update and add MakeComponent to everything

--- src/app/samples/everything.component.ts
+++ src/app/samples/everything.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {TableDataService} from '../data.service';
 import {SharkColumn} from 'shark-ng-table';
 import {ChildRowRenderingComponent} from './child-rows-rendering.component';
+import {MakeComponent} from "./custom-cell.component";
 
 @Component({
   template: `
@@ -45,7 +46,7 @@ export class EverythingComponent implements OnInit {
 
   tableColumns: SharkColumn[] = [
     { header: 'Year', property: 'year' },
-    { header: 'Make', property: 'make' },
+    { header: 'Make', property: 'make', component: MakeComponent },
     { header: 'Model', property: 'model' }
   ];
 
